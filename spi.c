d13f02eeb42f485789de5ba6e6304c2d9d36a0e4
Ahmed Abd El-Ghafar Mohammed <ahmed.batra2011@gmail.com>
2019-06-28 11:54:13 +0200

    Update spi.c

--- spi.c
+++ spi.c
@@ -3,17 +3,17 @@
 
 void SPI_Init_Advanced_Lib(spi_mode_ut master_slav, sample_bit_ut data_sample, clock_polarity_ut clock_idle, clock_select_ut transmit_edge){
     if(_SPI_DATA_SAMPLE_MIDDLE_L == data_sample){
-        SSPSTAT_REG.Register_Bits.Bit7 = 0; // Input data sampled at middle of data output time
+        SSPSTAT_REG.Register_Bits.Bit7 = 0; 	// Input data sampled at middle of data output time
     }
     else if(_SPI_DATA_SAMPLE_END_L == data_sample){
-        SSPSTAT_REG.Register_Bits.Bit7 = 1; // nput data sampled at end of data output time
+        SSPSTAT_REG.Register_Bits.Bit7 = 1; 	// nput data sampled at end of data output time
     }
     //================================================
     if(_SPI_MASTER_OSC_DIV4_L == master_slav){
-        SSPCON1_REG.Register_Bits.Bit0 = 0;	//  SPI Master mode, clock = FOSC/4
-        SSPCON1_REG.Register_Bits.Bit1 = 0; //  SPI Master mode, clock = FOSC/4
-        SSPCON1_REG.Register_Bits.Bit2 = 0; //  SPI Master mode, clock = FOSC/4
-        SSPCON1_REG.Register_Bits.Bit3 = 0; //  SPI Master mode, clock = FOSC/4
+        SSPCON1_REG.Register_Bits.Bit0 = 0; 	//  SPI Master mode, clock = FOSC/4
+        SSPCON1_REG.Register_Bits.Bit1 = 0; 	//  SPI Master mode, clock = FOSC/4
+        SSPCON1_REG.Register_Bits.Bit2 = 0; 	//  SPI Master mode, clock = FOSC/4
+        SSPCON1_REG.Register_Bits.Bit3 = 0; 	//  SPI Master mode, clock = FOSC/4
     }
     else if(_SPI_MASTER_OSC_DIV16_L == master_slav){
         SSPCON1_REG.Register_Bits.Bit0 = 1;	// SPI Master mode, clock = FOSC/16
@@ -47,8 +47,8 @@ void SPI_Init_Advanced_Lib(spi_mode_ut master_slav, sample_bit_ut data_sample, c
     }
     //================================================
     if(_SPI_CLK_IDLE_HIGH_L == clock_idle && _SPI_LOW_2_HIGH_L == transmit_edge){
-        SSPCON1_REG.Register_Bits.Bit4 = 1; // Clock LOW_2_HIGH
-        SSPSTAT_REG.Register_Bits.Bit6 = 1; // Clock LOW_2_HIGH
+        SSPCON1_REG.Register_Bits.Bit4 = 1; 	// Clock LOW_2_HIGH
+        SSPSTAT_REG.Register_Bits.Bit6 = 1; 	// Clock LOW_2_HIGH
     }
     else if(_SPI_CLK_IDLE_HIGH_L == clock_idle && _SPI_HIGH_2_LOW_L == transmit_edge){
         SSPCON1_REG.Register_Bits.Bit4 = 1;
@@ -67,27 +67,27 @@ void SPI_Init_Advanced_Lib(spi_mode_ut master_slav, sample_bit_ut data_sample, c
     //================================================
     if(_SPI_MASTER_OSC_DIV4_L == master_slav || _SPI_MASTER_OSC_DIV16_L == master_slav ||
        _SPI_MASTER_OSC_DIV64_L == master_slav || _SPI_MASTER_TMR2_L == master_slav){
-       ADCON1_REG.Register_Bits.Bit2 = 0; // A5 Digital
-       TRISC_REG.Register_Bits.Bit3 = 0; // SCK Output
-       TRISC_REG.Register_Bits.Bit4 = 1; // SDI Input
-       TRISC_REG.Register_Bits.Bit5 = 0; // SDO Output
-       TRISA_REG.Register_Bits.Bit5 = 0; // SS  Output
+       ADCON1_REG.Register_Bits.Bit2 = 0; 	// A5 Digital
+       TRISC_REG.Register_Bits.Bit3 = 0; 	// SCK Output
+       TRISC_REG.Register_Bits.Bit4 = 1; 	// SDI Input
+       TRISC_REG.Register_Bits.Bit5 = 0; 	// SDO Output
+       TRISA_REG.Register_Bits.Bit5 = 0; 	// SS  Output
     }
     else if(_SPI_SLAVE_SS_ENABLE_L == master_slav || _SPI_SLAVE_SS_DIS_L == master_slav){
-       ADCON1_REG.Register_Bits.Bit2 = 0; // A5 Digital
-       TRISC_REG.Register_Bits.Bit3 = 1; // SCK Input
-       TRISC_REG.Register_Bits.Bit4 = 1; // SDI Input
-       TRISC_REG.Register_Bits.Bit5 = 0; // SDO Output
-       TRISA_REG.Register_Bits.Bit5 = 1; // SS  Input
+       ADCON1_REG.Register_Bits.Bit2 = 0; 	// A5 Digital
+       TRISC_REG.Register_Bits.Bit3 = 1; 	// SCK Input
+       TRISC_REG.Register_Bits.Bit4 = 1; 	// SDI Input
+       TRISC_REG.Register_Bits.Bit5 = 0; 	// SDO Output
+       TRISA_REG.Register_Bits.Bit5 = 1; 	// SS  Input
     }
     
-    T_R_FLAG = _TRANSMISSION_RECEPTION_WAIT; // Celar Flag
+    T_R_FLAG = _TRANSMISSION_RECEPTION_WAIT; 	// Celar Flag
 }
 
 
 void SPI_Write_Lib(unsigned char val){
     uint8_t data_flush;		// To Clear the Buffer
-    SSPBUF_REG.Register = val; // Write the value
+    SSPBUF_REG.Register = val; 	// Write the value
     while(T_R_FLAG == _TRANSMISSION_RECEPTION_WAIT); // Wait the Flag
     T_R_FLAG = 0; // Clear The Flag
     data_flush = SSPBUF_REG.Register; // Clear the Buffer
